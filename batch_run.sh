1f54488b779dfc4437d31a5f6a76fdb447565355
Oliver Yongmin Knitter <knitter@gl-login1.arc-ts.umich.edu>
2022-03-31 21:04:04 -0400

    Batch 512 Tests.

--- batch_run.sh
+++ batch_run.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=vnls
 #SBATCH --mail-user=knitter@umich.edu
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=32g 
@@ -20,7 +20,7 @@ eval "$(conda shell.bash hook)"
 conda activate qmc
 module load gcc/8.2.0
 
-python -m main --config_file='./qaoa.yaml' TRAIN.OPTIMIZER_NAME sgd DATA.PROBLEM_TYPE vqls DATA.VECTOR_CHOICE constant TRAIN.APPLY_SR True DATA.NUM_SITES $1 TRAIN.BATCH_SIZE 1024 TRAIN.NUM_EPOCHS 10000 TRAIN.LEARNING_RATE 0.0005 DATA.NUM_CHAINS 8 MODEL.MODEL_NAME rbm_c
+python -m main --config_file='./qaoa.yaml' TRAIN.OPTIMIZER_NAME sgd DATA.PROBLEM_TYPE vqls DATA.VECTOR_CHOICE constant TRAIN.APPLY_SR True DATA.NUM_SITES $1 TRAIN.BATCH_SIZE 512 TRAIN.NUM_EPOCHS 2000 TRAIN.LEARNING_RATE 0.005 DATA.NUM_CHAINS 8 MODEL.MODEL_NAME rbm_c
 
 /bin/hostname
 sleep 60
